819a786c3996e85810e8888bfe710749e962fcc4
DariaBolmosova <dashabolmosova@gmail.com>
2019-11-04 19:23:05 +0200

    initial commit

---
+++ src/family/Mother.java
@@ -0,0 +1,7 @@
+package family;
+
+public class Mother {
+    int number_of_book;
+    int number_of_performance;
+    
+}
---
+++ src/family/Person.java
@@ -0,0 +1,8 @@
+package family;
+
+public class Person {
+
+    public static void main(String[] args) {
+
+    }
+}
